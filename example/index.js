284a79f5ed085ba865e96dc933e96e892c6c4d98
edge <functaire@gmail.com>
2016-11-09 19:49:17 -0800

    update example

--- example/index.js
+++ example/index.js
@@ -2,10 +2,17 @@ import { v } from '../src'
 import { connect, run } from '../src/redux-dom'
 import { createStore, combineReducers } from 'redux'
 
+// state layer
+
 let itemReducer = (state = [], action) => {
 	switch (action.type) {
 		case 'ADD_ITEM':
 			return state.concat(action.item)
+		case 'REMOVE_ITEM':
+			let { index } = action
+			let left = state.slice(0, index)
+			let right = state.slice(index + 1)
+			return left.concat(right)
 		default:
 			return state
 	}
@@ -25,29 +32,37 @@ let textReducer = (state = defaultText, action) => {
 
 let store = createStore(combineReducers({ items: itemReducer, text: textReducer }))
 
-let List = ({ items }) => (
-	<ol>
-		{ items.map(s => <ListItem>{ s }</ListItem>) }
-	</ol>
+// view layer
+
+let List = ({ items, remove }) => (
+	<div>
+		{ items.map((s, i) => <ListItem remove={ () => remove(i) }>{ s }</ListItem>) }
+	</div>
 )
 
-let ListItem = ({ children }) => <li>{ children }</li>
+let ListItem = ({ children, remove }) => (
+	<div>
+		{ children }
+		<button onclick={ remove }>×</button>
+	</div>
+)
 
-let ItemList = ({ items }) => <List>{ items }</List>
-
-let Root = ({ items, text, setValue, addItem }) => (
+let Root = ({ items, text, setValue, addItem, removeItem }) => (
 	<div>
 		<input oninput={ e => setValue(e.target.value) } onkeydown={ e => { if (e.keyCode === 13) addItem(text) } } value={ text } />
 		<button onclick={ () => addItem(text) }>Add Item</button>
-		<List items={ items } />
+		<List items={ items } remove={ removeItem }/>
 	</div>
 )
+
+// glue
 
 Root = connect(
 	state => ({ items: state.items, text: state.text }),
 	dispatch => ({
 		setValue: value => dispatch({ type: 'SET_VALUE', value }),
-		addItem: item => dispatch({ type: 'ADD_ITEM', item })
+		addItem: item => dispatch({ type: 'ADD_ITEM', item }),
+		removeItem: index => dispatch({ type: 'REMOVE_ITEM', index })
 	})
 )(Root)
 
